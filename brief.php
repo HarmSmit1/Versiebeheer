3ac1cc3945463c1359f521a068c00ed6f9bedaac
Jeroen Ho <Jeroenho@Live.nl>
2018-10-04 15:27:19 +0200

    php in brief toegevoegd

---
+++ brief.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+       
+		<p>Geachte,<?php $naamDocent ?></p>;
+		<p>Vandaag kan ik helaas niet aanwezig zijn op school, omdat <?php $naam ?>.;
+		Vanaf <?php $reden ?> hoop ik weer aanwezig te zijn op school.</p>;
+
+		<p>Met vriendelijke groet,</p>;
+		<p>(Invulstukje)</p>;
+		<p>(handtekening)</p>;
+
+     
+                
+    </body>
+</html>
